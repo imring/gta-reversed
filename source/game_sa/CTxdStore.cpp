a1d9e9e4fb768f2e368bf7da9ebde638c0215466
Pirulax <patrikjankovics7@gmail.com>
2021-08-17 04:45:03 +0200

    Apply format

--- source/game_sa/CTxdStore.cpp
+++ source/game_sa/CTxdStore.cpp
@@ -39,24 +39,36 @@ void CTxdStore::InjectHooks() {
 
 // initialise txd store
 // 0x731F20
-void CTxdStore::Initialise() { ((void(__cdecl*)())0x731F20)(); }
+void CTxdStore::Initialise() {
+    ((void(__cdecl*)())0x731F20)();
+}
 
 // shutdown txd store
 // 0x732000
-void CTxdStore::Shutdown() { ((void(__cdecl*)())0x732000)(); }
+void CTxdStore::Shutdown() {
+    ((void(__cdecl*)())0x732000)();
+}
 
 // 0x732060
-void CTxdStore::GameShutdown() { ((void(__cdecl*)())0x732060)(); }
+void CTxdStore::GameShutdown() {
+    ((void(__cdecl*)())0x732060)();
+}
 
 // 0x731930
-bool CTxdStore::StartLoadTxd(int index, RwStream* stream) { return ((bool(__cdecl*)(int, RwStream*))0x731930)(index, stream); }
+bool CTxdStore::StartLoadTxd(int index, RwStream* stream) {
+    return ((bool(__cdecl*)(int, RwStream*))0x731930)(index, stream);
+}
 
 // 0x731E40
-bool CTxdStore::FinishLoadTxd(int index, RwStream* stream) { return ((bool(__cdecl*)(int, RwStream*))0x731E40)(index, stream); }
+bool CTxdStore::FinishLoadTxd(int index, RwStream* stream) {
+    return ((bool(__cdecl*)(int, RwStream*))0x731E40)(index, stream);
+}
 
 // load txd from stream
 // 0x731DD0
-bool CTxdStore::LoadTxd(int index, RwStream* stream) { return ((bool(__cdecl*)(int, RwStream*))0x731DD0)(index, stream); }
+bool CTxdStore::LoadTxd(int index, RwStream* stream) {
+    return ((bool(__cdecl*)(int, RwStream*))0x731DD0)(index, stream);
+}
 
 // load txd from file
 // 0x7320B0
@@ -74,59 +86,89 @@ bool CTxdStore::LoadTxd(int index, char const* filename) {
 
 // store current txd
 // 0x7316A0
-void CTxdStore::PushCurrentTxd() { ((void(__cdecl*)())0x7316A0)(); }
+void CTxdStore::PushCurrentTxd() {
+    ((void(__cdecl*)())0x7316A0)();
+}
 
 // restore txd
 // 0x7316B0
-void CTxdStore::PopCurrentTxd() { ((void(__cdecl*)())0x7316B0)(); }
+void CTxdStore::PopCurrentTxd() {
+    ((void(__cdecl*)())0x7316B0)();
+}
 
 // set the txd to work with
 // 0x7319C0
-void CTxdStore::SetCurrentTxd(int index) { ((void(__cdecl*)(int))0x7319C0)(index); }
+void CTxdStore::SetCurrentTxd(int index) {
+    ((void(__cdecl*)(int))0x7319C0)(index);
+}
 
 // find txd by name. Returning value is txd index
 // 0x731850
-int CTxdStore::FindTxdSlot(char const* name) { return ((int(__cdecl*)(char const*))0x731850)(name); }
+int CTxdStore::FindTxdSlot(char const* name) {
+    return ((int(__cdecl*)(char const*))0x731850)(name);
+}
 
 // find txd by name hash. Returning value is txd index
 // 0x7318E0
-int CTxdStore::FindTxdSlot(unsigned int hash) { return ((signed int(__cdecl*)(unsigned int))0x7318E0)(hash); }
+int CTxdStore::FindTxdSlot(unsigned int hash) {
+    return ((signed int(__cdecl*)(unsigned int))0x7318E0)(hash);
+}
 
 // plugin-sdk has named it to getTexDictionary
-size_t CTxdStore::GetTxd(int index) { return plugin::CallAndReturn<size_t, 0x408340, int>(index); }
+size_t CTxdStore::GetTxd(int index) {
+    return plugin::CallAndReturn<size_t, 0x408340, int>(index);
+}
 
 // plugin-sdk has named it to getTXDEntryParentIndex
 // 0x408370
-int CTxdStore::GetParentTxdSlot(int index) { return plugin::CallAndReturn<int, 0x408370, int>(index); }
+int CTxdStore::GetParentTxdSlot(int index) {
+    return plugin::CallAndReturn<int, 0x408370, int>(index);
+}
 
 // create rw tex dictionary for txd slot
 // 0x731990
-void CTxdStore::Create(int index) { ((void(__cdecl*)(int))0x731990)(index); }
+void CTxdStore::Create(int index) {
+    ((void(__cdecl*)(int))0x731990)(index);
+}
 
 // allocate new slot for this txd
 // 0x731C80
-int CTxdStore::AddTxdSlot(char const* name) { return ((int(__cdecl*)(char const*))0x731C80)(name); }
+int CTxdStore::AddTxdSlot(char const* name) {
+    return ((int(__cdecl*)(char const*))0x731C80)(name);
+}
 
 // remove txd slot
 // 0x731CD0
-void CTxdStore::RemoveTxdSlot(int index) { ((void(__cdecl*)(int))0x731CD0)(index); }
+void CTxdStore::RemoveTxdSlot(int index) {
+    ((void(__cdecl*)(int))0x731CD0)(index);
+}
 
 // remove txd
 // 0x731E90
-void CTxdStore::RemoveTxd(int index) { ((void(__cdecl*)(int))0x731E90)(index); }
+void CTxdStore::RemoveTxd(int index) {
+    ((void(__cdecl*)(int))0x731E90)(index);
+}
 
 // add reference for txd
 // 0x731A00
-TxdDef* CTxdStore::AddRef(int index) { return ((TxdDef * (__cdecl*)(int))0x731A00)(index); }
+TxdDef* CTxdStore::AddRef(int index) {
+    return ((TxdDef * (__cdecl*)(int))0x731A00)(index);
+}
 
 // remove reference, if references count is 0, then delete txd
 // 0x731A30
-void CTxdStore::RemoveRef(int index) { ((void(__cdecl*)(int))0x731A30)(index); }
+void CTxdStore::RemoveRef(int index) {
+    ((void(__cdecl*)(int))0x731A30)(index);
+}
 
 // remove reference without deleting
 // 0x731A70
-TxdDef* CTxdStore::RemoveRefWithoutDelete(int index) { return ((TxdDef * (__cdecl*)(int))0x731A70)(index); }
+TxdDef* CTxdStore::RemoveRefWithoutDelete(int index) {
+    return ((TxdDef * (__cdecl*)(int))0x731A70)(index);
+}
 
 // get references count
 // 0x731AA0
-unsigned int CTxdStore::GetNumRefs(int index) { return ((unsigned int(__cdecl*)(int))0x731AA0)(index); }
+unsigned int CTxdStore::GetNumRefs(int index) {
+    return ((unsigned int(__cdecl*)(int))0x731AA0)(index);
+}
